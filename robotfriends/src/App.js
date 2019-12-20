5f421351c766c70b952f0b50a7a4638162638ca0
Mikenight120 <47368047+Mikenight120@users.noreply.github.com>
2019-12-19 20:20:31 -0400

    Finished Card Component and Search Component

--- robotfriends/src/App.js
+++ robotfriends/src/App.js
@@ -1,28 +1,18 @@
-import React , {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox.js";
+import { robots } from "./robots";
 
-class App extends Component {
-  render() {
-    return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-   );
-  }
-}
+const App = () => {
+  return (
+    <Fragment>
+      <div className="tc">
+        <h1>RobotFriends</h1>
+        <SearchBox />
+        <CardList robots={robots} />
+      </div>
+    </Fragment>
+  );
+};
 
 export default App;
---
+++ robotfriends/src/SearchBox.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+const SearchBox = () => {
+  return (
+    <Fragment>
+        <div className='pa2'>
+            <input className='pa2 b--green bg-lightest-blue' type="search" placeholder="search robots" />
+        </div>
+      
+    </Fragment>
+  );
+};
+
+export default SearchBox;
