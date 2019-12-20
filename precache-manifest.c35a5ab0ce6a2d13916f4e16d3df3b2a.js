69336f02dcbed73eb66c0ab334a973b5ea8ce0b7
JanMichaelSe <janm.montalvo@gmail.com>
2019-12-20 04:36:37 -0400

    Updates

---
+++ precache-manifest.c35a5ab0ce6a2d13916f4e16d3df3b2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17253f2ead26fa2cc31140d17f9e0a3",
+    "url": "/RobotFriends/index.html"
+  },
+  {
+    "revision": "be89d948ce5d9a35aabb",
+    "url": "/RobotFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ade5a04d6f48a017b6b9",
+    "url": "/RobotFriends/static/css/main.413cacc4.chunk.css"
+  },
+  {
+    "revision": "be89d948ce5d9a35aabb",
+    "url": "/RobotFriends/static/js/2.fbfdd1bc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RobotFriends/static/js/2.fbfdd1bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ade5a04d6f48a017b6b9",
+    "url": "/RobotFriends/static/js/main.f6e03dcc.chunk.js"
+  },
+  {
+    "revision": "216aae5e4ccd153c4ac5",
+    "url": "/RobotFriends/static/js/runtime-main.a1576d44.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
